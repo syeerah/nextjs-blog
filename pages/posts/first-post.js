bc0af11ea0da8e81a9911c3bc82fadf9c0030e62
syeerah <nrrinsyeerh@gmail.com>
2023-04-10 21:52:21 +0900

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <h2>My name is Aina Nur Insyeerah Binti Abd Rahman.</h2>
+      <h3>I came from Malaysia.</h3>
+      <h4>I love EXO and hopefully one day I get to go to their concert.</h4>
+        <Link href="/">Back to home</Link>
+    
+    </>
+  );
+}
+
